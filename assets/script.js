a29814343808740248345a27fd6d737e3d6cc405
Drzazga88 <drzewiecka.katarzyna88@gmail.com>
2023-02-08 17:45:56 +0000

    re-written JS, not finished yet

--- assets/script.js
+++ assets/script.js
@@ -1,127 +1,177 @@
-var API_Key = "5111297b359fd6c2201ce081633e2699";
-var API_URL = `https://api.openweathermap.org/data/2.5/weather?appid=${API_Key}&units=metric`;
-var API_URL_FORECAST = `https://api.openweathermap.org/data/2.5/forecast?appid=${API_Key}&units=metric`;
-
-// Current day weather
-const getWeather = (city) => {
-  return $.ajax({
-    url: `${API_URL}&q=${city}`,
-    method: "GET",
-  });
-};
-
-const displayWeather = (city) => {
-  // Get the current search history from local storage
-  let searchHistory = JSON.parse(localStorage.getItem("searchHistory")) || [];
-
-  // Add the current search to the history
-  searchHistory.push(city);
-
-  // Store the updated search history in local storage
-  localStorage.setItem("searchHistory", JSON.stringify(searchHistory));
-
-  getWeather(city)
-    .done((weatherData) => {
-      const cityName = weatherData.name;
-      const date = new Date();
-      const weatherIcon = `http://openweathermap.org/img/wn/${weatherData.weather[0].icon}@2x.png`;
-      const temperature = weatherData.main.temp;
-      const humidity = weatherData.main.humidity;
-      const windSpeed = weatherData.wind.speed;
-
-      // Update the HTML to display the information
-      document.getElementById("city-name").textContent = cityName;
-      document.getElementById(
-        "date"
-      ).textContent = `( ${date.toDateString()} )`;
-      document.getElementById("weather-icon").src = weatherIcon;
-      document.getElementById(
-        "temperature"
-      ).textContent = `Temperature: ${temperature}°C`;
-      document.getElementById(
-        "humidity"
-      ).textContent = `Humidity: ${humidity}%`;
-      document.getElementById(
-        "wind-speed"
-      ).textContent = `Wind Speed: ${windSpeed} mph`;
-    })
-    .fail((error) => {
-      console.error(`Error retrieving weather data: ${error}`);
-    });
-};
-
-// Add user input for the city
 $(document).ready(function () {
+  // Add user input for the city
   $("form").submit(function (event) {
     event.preventDefault();
+    const API_KEY = "5111297b359fd6c2201ce081633e2699";
     const city = $("#search-input").val();
-    displayWeather(city);
-  });
-});
+    const API_URL = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`;
 
-// Data for 5 days forecast
-const getForecast = (city) => {
-  return $.ajax({
-    url: `${API_URL_FORECAST}&q=${city}`,
-    method: "GET",
-  });
-};
+    // Retrieve geographical coordinates given a city name
+    $.get(API_URL, function (data) {
+      console.log(data); // control step
+      const lat = data.coord.lat;
+      const lon = data.coord.lon;
+      const API_URL_COORD = `http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
 
-// const dateOne = moment().add(1, 'days').format('L');
-// document.querySelector("#day1-date").innerHTML = dateOne;
-$("form").submit(function (event) {
-    event.preventDefault();
-    const city = $("#search-input").val();
-    displayWeather(city);
+      // Retrieve required weather data from API
+      $.get(API_URL_COORD, function (coordData) {
+        console.log(coordData); // control step
+        const cityName = coordData.name;
+        const date = new Date();
+        const weatherIcon = `http://openweathermap.org/img/wn/${coordData.weather[0].icon}@2x.png`;
+        const temperature = coordData.main.temp;
+        const humidity = coordData.main.humidity;
+        const windSpeed = coordData.wind.speed;
+        console.log(
+          cityName,
+          date,
+          weatherIcon,
+          temperature,
+          humidity,
+          windSpeed
+        ); // control step
 
-// Call the API and retrieve the data
-fetch(
-  `https://api.openweathermap.org/data/2.5/forecast?q=${city}&units=metric&appid=${API_Key}`
-)
-  .then((response) => response.json())
-  .then((data) => {
-    // Populate the values for the 5-day forecast
-    console.error("Data from API: ", data);
-    for (let i = 1; i <= 5; i++) {
-      const forecast = data.list[i];
-      console.log("Forecast data: ", forecast);
-      document.querySelector(`#day${i}-date`).innerHTML = new Date(forecast.dt * 1000).toDateString();
-      document.querySelector(`#day${i}-icon`).src = `https://openweathermap.org/img/wn/${forecast.weather[0].icon}@2x.png`;
-      document.querySelector(`#day${i}-temp`).innerHTML = `Temperature: ${forecast.main.temp} °C`;
-      document.querySelector(`#day${i}-hum`).innerHTML = `Humidity: ${forecast.main.humidity}%`;
-      document.querySelector(`#day${i}-wind`).innerHTML = `Wind Speed: ${forecast.wind.speed} m/s`;
-    }
-  })
-  .catch((error) => console.error(error));
-});
+        // Update the HTML elements with the weather data from API retrieved in the previous step
+        document.getElementById("city-name").textContent = cityName;
+        document.getElementById(
+          "date"
+        ).textContent = `( ${date.toDateString()} )`;
+        document.getElementById("weather-icon").src = weatherIcon;
+        document.getElementById(
+          "temperature"
+        ).textContent = `Temperature: ${temperature}°C`;
+        document.getElementById(
+          "humidity"
+        ).textContent = `Humidity: ${humidity}%`;
+        document.getElementById(
+          "wind-speed"
+        ).textContent = `Wind Speed: ${windSpeed} mph`;
 
-const displaySearchHistory = (searchHistory) => {
-  // A reference for the search history
-  const historyContainer = document.querySelector("#history");
-
-  // Clear any existing search history from the history
-  historyContainer.innerHTML = "";
-
-  // Only show the last 5 items in the search history
-  const limitedHistory = searchHistory.slice(-5);
-
-  // Loop over each item in the search history
-  for (const city of limitedHistory) {
-    // Create a new HTML element for the city
-    const cityElement = document.createElement("a");
-    cityElement.textContent = city;
-    cityElement.classList.add("list-group-item");
-    cityElement.href = "#";
-
-    // Add an event listener to the city element to display the weather when it's clicked
-    cityElement.addEventListener("click", () => {
-      displayWeather(city);
+        // Forecast data fetch from API
+        const API_URL_FORECAST = `http://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
+        $.get(API_URL_FORECAST, function (forecastData) {
+          console.log(forecastData); // control step
+          // Loop to retrieve data for 5 days weather forecast from API
+          for (let i = 0; i < forecastData.list.length; i += 8) {
+            const forecast = forecastData.list[i];
+            const forecastDate = new Date(forecast.dt * 1000);
+            const forecastWeatherIcon = `http://openweathermap.org/img/wn/${forecast.weather[0].icon}@2x.png`;
+            const forecastTemperature = forecast.main.temp;
+            const forecastHumidity = forecast.main.humidity;
+            const forecastWind = forecast.wind.speed;
+            console.log(
+              forecastDate,
+              forecastWeatherIcon,
+              forecastTemperature,
+              forecastHumidity
+            ); // control step
+            // Update the HTML elements with the forecast data
+            switch (i) {
+              case 0:
+                $("#day1-date").text(`${forecastDate.toDateString()}`);
+                $("#day1-icon").attr("src", forecastWeatherIcon);
+                $("#day1-temp").text(
+                  "Temperature: " + forecastTemperature + "°C"
+                );
+                $("#day1-hum").text("Humidity: " + forecastHumidity + "%");
+                $("#day1-wind").text("Wind speed: " + forecastWind + "m/s");
+                break;
+              case 8:
+                $("#day2-date").text(`${forecastDate.toDateString()}`);
+                $("#day2-icon").attr("src", forecastWeatherIcon);
+                $("#day2-temp").text(
+                  "Temperature: " + forecastTemperature + "°C"
+                );
+                $("#day2-hum").text("Humidity: " + forecastHumidity + "%");
+                $("#day2-wind").text("Wind speed: " + forecastWind + "m/s");
+                break;
+              case 16:
+                $("#day3-date").text(`${forecastDate.toDateString()}`);
+                $("#day3-icon").attr("src", forecastWeatherIcon);
+                $("#day3-temp").text(
+                  "Temperature: " + forecastTemperature + "°C"
+                );
+                $("#day3-hum").text("Humidity: " + forecastHumidity + "%");
+                $("#day3-wind").text("Wind speed: " + forecastWind + "m/s");
+                break;
+              case 24:
+                $("#day4-date").text(`${forecastDate.toDateString()}`);
+                $("#day4-icon").attr("src", forecastWeatherIcon);
+                $("#day4-temp").text(
+                  "Temperature: " + forecastTemperature + "°C"
+                );
+                $("#day4-hum").text("Humidity: " + forecastHumidity + "%");
+                $("#day4-wind").text("Wind speed: " + forecastWind + "m/s");
+                break;
+              case 32:
+                $("#day5-date").text(`${forecastDate.toDateString()}`);
+                $("#day5-icon").attr("src", forecastWeatherIcon);
+                $("#day5-temp").text(
+                  "Temperature: " + forecastTemperature + "°C"
+                );
+                $("#day5-hum").text("Humidity: " + forecastHumidity + "%");
+                $("#day5-wind").text("Wind speed: " + forecastWind + "m/s");
+                break;
+            }
+          }
+          // Extract the necessary data from the API response and update the UI
+        }).fail(function () {
+          console.error(
+            "An error occurred while fetching the 5-day forecast data"
+          );
+          // Handle the error and show an appropriate message to the user
+        });
+      }).fail(function () {
+        console.error(
+          "An error occurred while fetching the weather data based on coordinates"
+        );
+        // Handle the error and show an appropriate message to the user
+      });
+    }).fail(function () {
+      console.error("An error occurred while fetching the weather data");
+      // Handle the error and show an appropriate message to the user
     });
+  });
 
-    // Append the city element to the history container
-    historyContainer.appendChild(cityElement);
-  }
-};
-// Retrieve the search history from local storage
-const searchHistory = JSON.parse(localStorage.getItem("searchHistory")) || []; //if the search history does not exist, an empty array is assigned
-displaySearchHistory(searchHistory);
+  // const displayWeather = (city) => {
+  //   // Get the current search history from local storage
+  //   let searchHistory = JSON.parse(localStorage.getItem("searchHistory")) || [];
+
+  //   // Add the current search to the history
+  //   searchHistory.push(city);
+
+  //   // Store the updated search history in local storage
+  //   localStorage.setItem("searchHistory", JSON.stringify(searchHistory));
+  // };
+
+  //   const displaySearchHistory = (searchHistory) => {
+  //       // A reference for the search history
+  //       const historyContainer = document.querySelector("#history");
+
+  //       // Clear any existing search history from the history
+  //       historyContainer.innerHTML = "";
+
+  //       // Only show the last 5 items in the search history
+  //       const limitedHistory = searchHistory.slice(-5);
+
+  //       // Loop over each item in the search history
+  //       for (const city of limitedHistory) {
+  //         // Create a new HTML element for the city
+  //         const cityElement = document.createElement("a");
+  //         cityElement.textContent = city;
+  //         cityElement.classList.add("list-group-item");
+  //         cityElement.href = "#";
+
+  //         // Add an event listener to the city element to display the weather when it's clicked
+  //         cityElement.addEventListener("click", () => {
+  //           displayWeather(city);
+  //         });
+
+  //         // Append the city element to the history container
+  //         historyContainer.appendChild(cityElement);
+  //       }
+  //     };
+  //     // Retrieve the search history from local storage
+  //     const searchHistory = JSON.parse(localStorage.getItem("searchHistory")) || []; //if the search history does not exist, an empty array is assigned
+  //  displaySearchHistory(searchHistory);
+});
